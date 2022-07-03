215295a26b54f18ecefd88ef3bc5f32dfd9dade1
MandeNaren <y2pgftpe@gmail.com>
2022-07-03 23:01:07 +0900

    graphql-client-tutorial

---
+++ graphql-client-tutorial/frontend/src/App.jsx
@@ -0,0 +1,42 @@
+// npm run devで起動
+
+import './App.css';
+import {useQuery,gql} from "@apollo/client";
+
+const BOOKS = gql`
+  query{
+    books{
+      title
+      author
+    }
+  }
+`
+
+function Books() {
+  const {loading, error, data} = useQuery(BOOKS);
+
+  if(loading) return <p>Loading...</p>;
+  if(error) return <p>Error</p>;
+  console.log(data);
+
+  return data.books.map(({title, author}) => (
+    <div key = {title}>
+      <p>
+        {author} : {title}
+      </p>
+    </div>
+  ))
+}
+
+
+function App() {
+
+  return (
+    <div className="App">
+      <h2>Graphql Client</h2>
+      <Books />
+    </div>
+  )
+}
+
+export default App
