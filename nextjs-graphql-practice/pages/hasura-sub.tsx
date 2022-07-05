ce4fe7004db0e83ce2bfcab49eb38b583277c6b5
MandeNaren <y2pgftpe@gmail.com>
2022-07-05 12:58:37 +0900

    nextjs-practice-testまで

---
+++ nextjs-graphql-practice/pages/hasura-sub.tsx
@@ -0,0 +1,34 @@
+import { VFC } from 'react'
+import Link from 'next/link'
+import { useQuery } from '@apollo/client'
+import { GET_USERS_LOCAL, GET_USERS } from '../queries/queries'
+import { GetUsersLocalQuery } from '../types/generated/graphql'
+import { Layout } from '../components/Layout'
+
+const FetchSub: VFC = () => {
+    const { data,error } = useQuery<GetUsersLocalQuery>(GET_USERS_LOCAL)
+
+    if (error)
+    return (
+    <Layout title="Hasura fetchPolicy">
+        <p>Error: {error.message}</p>
+    </Layout>
+    )
+    
+    return (
+        <Layout title="Hasura fetchPolicy read cache">
+            <p className="mb-6 font-bold">Direct read out from cache</p>
+            {data?.users.map((user) => {
+                return (
+                <p className="my-1" key={user.id}>
+                    {user.name}
+                </p>
+                )
+            })}
+            <Link href="/hasura-main">
+                <a className="mt-6">Back</a>
+            </Link>
+        </Layout>
+    )
+}
+export default FetchSub
--- nextjs-graphql-practice/tailwind.config.js
+++ nextjs-graphql-practice/tailwind.config.js
@@ -8,7 +8,7 @@ module.exports = {
       extend: {},
   },
   variants: {
-      extend: {},
+      extend: {opacity:['disabled']},
   },
   plugins: [],
 }
